836571e2a340bb2e7b1ff8298a654054e09c4600
itu_stc <aelrawy29@gmail.com>
2025-12-17 11:53:28 +0300

    chore: enhamc/update printer sizes

--- src/app/shared/components/printing-size-dimensions/models/printing-size-dimensions.model.ts
+++ src/app/shared/components/printing-size-dimensions/models/printing-size-dimensions.model.ts
@@ -1,5 +1,5 @@
-export type PrintingSizeTypes = 'A6' | 'A5' | 'A4' | '5x5';
-export type PrintingSizeLabelTypes = 'A6' | 'A5' | 'A4' | '5x5';
+export type PrintingSizeTypes = 'A6' | 'A5' | 'A4' | '5x5' |'9x11';
+export type PrintingSizeLabelTypes = 'A6' | 'A5' | 'A4' | '5x5' |'9x11';
 
 export interface PrintingSizeDimension {
   label: PrintingSizeLabelTypes;

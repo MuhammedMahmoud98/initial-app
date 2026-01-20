9bfd2f9d58bfb5dc46a332486d88fe5b287173aa
Muhammed Mahmoud <57262428+MuhammedMahmoud98@users.noreply.github.com>
2026-01-20 11:31:16 +0200

    feat: implement main widget cards with dynamic icons and loading states

--- src/app/core/components/statistics-widgets/statistics-widgets.component.ts
+++ src/app/core/components/statistics-widgets/statistics-widgets.component.ts
@@ -1,11 +1,14 @@
-import {ChangeDetectionStrategy, Component, signal, afterNextRender, AfterRenderRef} from '@angular/core';
+import {ChangeDetectionStrategy, Component, afterNextRender, AfterRenderRef, inject} from '@angular/core';
 import {Skeleton} from 'primeng/skeleton';
 import {animate, query, stagger, style, transition, trigger} from '@angular/animations';
+import {StatisticMainCardsService} from './services/statistic-main-cards.service';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-statistics-widgets',
   imports: [
-    Skeleton
+    Skeleton,
+    NgOptimizedImage
   ],
   standalone: true,
   templateUrl: './statistics-widgets.component.html',
@@ -13,28 +16,30 @@ import {animate, query, stagger, style, transition, trigger} from '@angular/anim
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
     trigger('listAnimation', [
-      transition(':enter, * => *', [
+      transition('* <=> *', [
         // find entering children and apply staggered animation
-        query(':enter', [
+        query('*', [
           style({ opacity: 0, transform: 'translateY(10px)' }),
           stagger(200, [
-            animate('920ms cubic-bezier(.2,.8,.2,1)', style({ opacity: 1, transform: 'translateY(0)' }))
+            animate('500ms cubic-bezier(.2,.8,.2,1)', style({ opacity: 1, transform: 'translateY(0)' }))
           ])
         ], { optional: true })
       ])]),
     trigger('valueAnimation', [
       transition(':enter', [
         style({ opacity: 0, transform: 'translateY(8px)' }),
-        animate('920ms 400ms cubic-bezier(.2,.8,.2,1)', style({ opacity: 1, transform: 'translateY(0)' }))
+        animate('920ms 800ms cubic-bezier(.2,.8,.2,1)', style({ opacity: 1, transform: 'translateY(0)' }))
       ])])
   ]
 })
 export class StatisticsWidgetsComponent {
-  isLoading = signal(true);
+  // INJECTIONS
+  readonly statisticMainCardsService: StatisticMainCardsService = inject(StatisticMainCardsService);
+  // SIGNALS
+  mainCardsVM = this.statisticMainCardsService.mainCardsVM;
+  isLoading = this.statisticMainCardsService.isLoading;
 
   init: AfterRenderRef = afterNextRender(() => {
-    setTimeout(() => {
-      this.isLoading.set(false);
-    }, 1500);
+    this.statisticMainCardsService.loadMainCardsStatistics();
   });
 }
